6ad30249eac94ff8b1f431f4f038d27b05b7af91
daniel-life <Daniel08222003@gmail.com>
2023-12-29 23:51:56 +0800

    added files for vercel deployment

--- dan_supermarket/dan_supermarket/settings.py
+++ dan_supermarket/dan_supermarket/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sgy(4@^hiv*^z$a-81ohal59rg6@t1y%=@zgq3%-s1&vsrlrol'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
